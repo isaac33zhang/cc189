c3dbe33ce190bc3ca342c902b5c8887821e34212
Zhang <fanzhaf@f45c89a66713.ant.amazon.com>
2018-08-18 10:42:46 -0700

    Is Unique

--- src/com/cc189/Main.java
+++ src/com/cc189/Main.java
@@ -1,8 +1,18 @@
 package com.cc189;
 
+import com.cc189.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        // test IsUnique
+        String[] inputs = {"a", "abc", "abb"};
+        boolean[] results = {true, true, false};
+        IsUnique isUnique = new IsUnique();
+        for (int i = 0; i < inputs.length; i++) {
+            boolean result = isUnique.isUniqueCars(inputs[i]);
+            assert result == results[i];
+        }
     }
 }
