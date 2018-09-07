2b5e8a7ac0270d5bb7a94dfdc4b2a08de719247e
Fan Zhang <fanzhaf@amazon.com>
2018-09-06 23:06:16 -0700

    First Common Ancestor optimized

--- src/com/cc189/FirstCommonAncestor.java
+++ src/com/cc189/FirstCommonAncestor.java
@@ -22,4 +22,37 @@ class FirstCommonAncestor {
         if (root == p) return true;
         return covers(root.left, p) || covers(root.right, p);
     }
+
+    TreeNode commonAncestorOptimized(TreeNode root, TreeNode p, TreeNode q) {
+        Result r = commonAncestorHelper(root, p, q);
+        if (r.isAncestor) { return r.node; }
+        return null;
+    }
+
+    Result commonAncestorHelper(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) { return new Result(null, false); }
+        if (root == p && root == q) { return new Result(root, true); }
+        Result left = commonAncestorHelper(root.left, p, q);
+        if (left.isAncestor) { return left; }
+        Result right = commonAncestorHelper(root.right, p, q);
+        if (right.isAncestor) { return right; }
+        if (root == p || root == q) {
+            boolean isAnc = left.node != null || right.node != null;
+            return new Result(root, isAnc);
+        } else if (left.node != null && right.node != null) {
+            return new Result(root, true);
+        } else {
+            return new Result(left.node == null ? right.node : left.node, false);
+        }
+    }
 }
+
+class Result{
+    TreeNode node;
+    boolean isAncestor;
+    public Result(TreeNode n, boolean isAnc) {
+        node = n;
+        isAncestor = isAnc;
+    }
+}
+
